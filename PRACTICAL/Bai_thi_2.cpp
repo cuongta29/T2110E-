ff8c34095e1af51662ef6d8ff651b5d95ad18575
cuong <cuongtaviet51@gmail.com>
2021-11-12 19:26:15 +0700

    NOP BAI

---
+++ PRACTICAL/Bai_thi_2.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h> 
+float average (int arr[], int number) {
+    int sum = 0;
+    int count = 0;
+    float average = 0;
+    for (int i = 0; i < number; i++) {
+        sum += arr[i];
+        count++;
+    }
+    average = sum / count;
+    return  average;
+}
+
+int main() {
+    int size;
+    printf("Nhap so luong so nguyen: ");
+    scanf("%d", &size);
+    
+    int arr[size];
+    
+    for (int i = 0; i < size; i++) {
+        printf("So nguyen thu %d: ", i);
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Trung binh cong: %.2f\n", average(arr, size));
+    return 0;
+}
